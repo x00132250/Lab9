810bb56d7e41058047739d827bbba2c23db8ae07
Enda Lee <enda.lee@ittdublin.ie>
2016-11-03 01:11:38 +0000

    Added model class

---
+++ app/models/Product.java
@@ -0,0 +1,65 @@
+package models
+
+public class Product {
+
+    // Properties
+
+    private Long id;
+    private String name;
+    private String description;
+    private int	stock;
+    private double price;
+
+    // Default constructor
+    public  Product() {
+    }
+
+    // Constructor to initialise object
+    public  Product(Long id, String name, String description, int stock, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.stock = stock;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
