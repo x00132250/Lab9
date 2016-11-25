1def478dc939e14321bcaead355bfda1f8b93595
IanDaly <iandaly74@gmail.com>
2016-11-25 16:04:31 +0000

    finished lab9

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,6 +17,21 @@ import models.*;
 
 public class HomeController extends Controller {
 
+    private FormFactory formFactory;
+
+    @Inject
+    public HomeController(FormFactory f){
+        this.formFactory = f;
+    }
+
+    public Result addProduct(){
+
+        Form<Product> addProductForm = formFactory.form(Product.class);
+
+        return ok(addProduct.render(addProductForm));
+
+    }
+
     public Result index(String name) {
         return ok(index.render("Welcome to the Home page", name));
     }
@@ -32,6 +47,29 @@ public class HomeController extends Controller {
         return ok(products.render(productsList));
     }
 
+    public Result deleteProduct(Long id){
+        Product.find.ref(id).delete();
+        flash("success", "Product has been deleted");
+        return redirect(routes.HomeController.products());
+    }
+
+    public Result addProductSubmit(){
+
+        Form<Product> newProductForm = formFactory.form(Product.class).bindFromRequest();
+
+        if(newProductForm.hasErrors()){
+
+            return badRequest(addProduct.render(newProductForm));
+        }
+
+        Product newProduct = newProductForm.get();
+
+        newProduct.save();
+
+        flash("success Product " + newProduct.getName() + " has been created");
+
+        return redirect(controllers.routes.HomeController.products());
+    }
 }
 
 
